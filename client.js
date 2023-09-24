0e9ba50fd75a16d884a9a13a1383a00c9e7c37a4
rafapaezbas <rafa@holepunch.com>
2023-09-24 16:01:33 +0200

    renamed server->receiver, client->sender

--- client.js
+++ client.js
@@ -1,24 +1,4 @@
-const net = require('net')
-
-class Client {
+module.exports = class Client {
   constructor () {
-    this._client = new net.Socket()
-  }
-
-  async connect (port, address = '127.0.0.1') {
-    return new Promise((resolve) => this._client.connect(1337, '127.0.0.1', resolve))
-  }
-
-  write (msg) {
-    this._client.write(this._format(msg))
-  }
-
-  _format (msg) {
-    const buffer = Buffer.from(msg)
-    const length = Buffer.alloc(4) // UInt32 bytes
-    length.writeUInt32BE(buffer.length)
-    return Buffer.concat([length, buffer])
   }
 }
-
-module.exports = { Client }
--- test/index.test.js
+++ test/index.test.js
@@ -1,58 +1,58 @@
-const { Server, Session } = require('../server.js')
-const { Client } = require('../client.js')
+const { Receiver, Session } = require('../lib/receiver.js')
+const { Sender } = require('../lib/sender.js')
 const test = require('brittle')
 
 test('messages format', async (t) => {
-  const server = new Server()
-  const client = new Client()
+  const receiver = new Receiver()
+  const sender = new Sender()
   const session = new Session()
   t.plan(3)
 
-  const payload = Buffer.from('hello from client')
-  server.on('message', rcv => {
+  const payload = Buffer.from('hello from receiver')
+  receiver.on('message', rcv => {
     t.ok(payload.equals(rcv))
     t.is(session.buffer.length, 0)
     t.is(session.messageLength, null)
   })
 
-  server._ondata(client._format(payload), session)
+  receiver._ondata(sender._format(payload), session)
 })
 
 test('splitted message', async (t) => {
-  const server = new Server()
-  const client = new Client()
+  const receiver = new Receiver()
+  const sender = new Sender()
   const session = new Session()
   t.plan(3)
 
-  const payload = Buffer.from('hello again from client, message splited')
-  const msg = client._format(payload)
+  const payload = Buffer.from('hello again from receiver, message splited')
+  const msg = sender._format(payload)
   const msgA = msg.slice(0, Math.floor(msg.length / 2))
   const msgB = msg.slice(Math.floor(msg.length / 2), msg.length)
 
-  server.on('message', rcv => {
+  receiver.on('message', rcv => {
     t.ok(payload.equals(rcv))
     t.is(session.buffer.length, 0)
     t.is(session.messageLength, null)
   })
 
-  server._ondata(msgA, session)
-  server._ondata(msgB, session)
+  receiver._ondata(msgA, session)
+  receiver._ondata(msgB, session)
 })
 
 test('multi message', async (t) => {
-  const server = new Server()
-  const client = new Client()
+  const receiver = new Receiver()
+  const sender = new Sender()
   const session = new Session()
   t.plan(6)
 
   const payloadA = Buffer.from('this is payloadA')
   const payloadB = Buffer.from('and this is payloadB')
-  const msgA = client._format(payloadA)
-  const msgB = client._format(payloadB)
+  const msgA = sender._format(payloadA)
+  const msgB = sender._format(payloadB)
 
   let messages = 0
 
-  server.on('message', rcv => {
+  receiver.on('message', rcv => {
     if (messages === 0) {
       t.ok(rcv.equals(payloadA))
       messages++
@@ -63,5 +63,5 @@ test('multi message', async (t) => {
     t.is(session.messageLength, null)
   })
 
-  server._ondata(Buffer.concat([msgA, msgB]), session)
+  receiver._ondata(Buffer.concat([msgA, msgB]), session)
 })
